674a398c03b811eb38a5c2a95d04f87ceff6143e
John White <jowhite@firstam.com>
2013-07-05 10:46:03 -0400

    Added Validation attribute builders.

---
+++ Hex/AttributeBuilders/ValidationAttributeBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hex.AttributeBuilders
+{
+	public abstract class ValidationAttributeBuilder<TSelf>
+		: HtmlAttributeBuilder<TSelf>
+		where TSelf : ValidationAttributeBuilder<TSelf>
+	{
+	}
+}
