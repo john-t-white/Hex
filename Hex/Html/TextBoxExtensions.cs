760dd57179cc9aeb3eba0a9ae65b546a2a40d116
John White <jowhite@firstam.com>
2013-07-05 15:45:01 -0400

    Added TextBoxExtensions.

---
+++ Hex/Html/TextBoxExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Hex.AttributeBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using Hex.Extensions;
+using System.Linq.Expressions;
+
+namespace Hex.Html
+{
+	public static class TextBoxExtensions
+	{
+		public static MvcHtmlString TextBox( this HtmlHelper htmlHelper, string name, Action<TextBoxAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.TextBox( name, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString TextBox( this HtmlHelper htmlHelper, string name, string value, Action<TextBoxAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.TextBox( name, value, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString TextBox( this HtmlHelper htmlHelper, string name, string value, string format, Action<TextBoxAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.TextBox( name, value, format, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString TextBoxFor<TModel, TProperty>( this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, Action<TextBoxAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.TextBoxFor( expression, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString TTextBoxFor<TModel, TProperty>( this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, string format, Action<TextBoxAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.TextBoxFor( expression, format, attributeExpression.GetAttributes() );
+		}
+	}
+}
