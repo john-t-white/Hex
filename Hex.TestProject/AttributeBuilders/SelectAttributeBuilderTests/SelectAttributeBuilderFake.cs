fed72bfbc25e66539dfd590fcef059dc9293226b
John White <jowhite@firstam.com>
2013-07-05 12:02:36 -0400

    Added DropDownListAttributeBuilder and ListBoxAttributeBuilder.

---
+++ Hex.TestProject/AttributeBuilders/SelectAttributeBuilderTests/SelectAttributeBuilderFake.cs
@@ -0,0 +1,14 @@
+﻿using Hex.AttributeBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hex.TestProject.AttributeBuilders.SelectAttributeBuilderTests
+{
+	public class SelectAttributeBuilderFake
+		: SelectAttributeBuilder<SelectAttributeBuilderFake>
+	{
+	}
+}
---
+++ Hex/AttributeBuilders/SelectAttributeBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Hex.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hex.AttributeBuilders
+{
+	public abstract class SelectAttributeBuilder<TSelf>
+		: InputAttributeBuilder<TSelf>
+		where TSelf : SelectAttributeBuilder<TSelf>
+	{
+		public TSelf Size( int size )
+		{
+			this.Attributes[ HtmlAttributes.Size ] = size;
+
+			return ( TSelf )this;
+		}
+	}
+}
