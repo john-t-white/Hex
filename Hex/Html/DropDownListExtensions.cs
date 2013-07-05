4f41792ffa0bfc2a88f3bb1929efb72c95949fe0
John White <jowhite@firstam.com>
2013-07-05 15:23:56 -0400

    Added DropDownListExtensions. Added ListBoxExtensions.

---
+++ Hex/Html/DropDownListExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using Hex.Extensions;
+using Hex.AttributeBuilders;
+using System.Linq.Expressions;
+
+namespace Hex.Html
+{
+	public static class DropDownListExtensions
+	{
+		public static MvcHtmlString DropDownList( this HtmlHelper htmlHelper, string name, Action<DropDownListAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.DropDownList( name, null, null, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString DropDownList( this HtmlHelper htmlHelper, string name, string optionLabel, Action<DropDownListAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.DropDownList( name, null, optionLabel, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString DropDownList( this HtmlHelper htmlHelper, string name, IEnumerable<SelectListItem> selectList, Action<DropDownListAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.DropDownList( name, selectList, null, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString DropDownList( this HtmlHelper htmlHelper, string name, IEnumerable<SelectListItem> selectList, string optionLabel, Action<DropDownListAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.DropDownList( name, selectList, optionLabel, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString DropDownListFor<TModel, TProperty>( this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, Action<DropDownListAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.DropDownListFor<TModel, TProperty>( expression, null, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString DropDownListFor<TModel, TProperty>( this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, IEnumerable<SelectListItem> selectList, Action<DropDownListAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.DropDownListFor<TModel, TProperty>( expression, selectList, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString DropDownListFor<TModel, TProperty>( this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, IEnumerable<SelectListItem> selectList, string optionLabel, Action<DropDownListAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.DropDownListFor<TModel, TProperty>( expression, selectList, optionLabel, attributeExpression.GetAttributes() );
+		}
+	}
+}
---
+++ Hex/Html/ListBoxExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Hex.AttributeBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using Hex.Extensions;
+using System.Linq.Expressions;
+
+namespace Hex.Html
+{
+	public static class ListBoxExtensions
+	{
+		public static MvcHtmlString ListBox( this HtmlHelper htmlHelper, string name, Action<ListBoxAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.ListBox( name, null, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString ListBox( this HtmlHelper htmlHelper, string name, IEnumerable<SelectListItem> selectList, Action<ListBoxAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.ListBox( name, selectList, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString ListBoxFor<TModel, TProperty>( this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, Action<ListBoxAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.ListBoxFor<TModel, TProperty>( expression, null, attributeExpression.GetAttributes() );
+		}
+
+		public static MvcHtmlString ListBoxFor<TModel, TProperty>( this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, IEnumerable<SelectListItem> selectList, Action<ListBoxAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.ListBoxFor<TModel, TProperty>( expression, selectList, attributeExpression.GetAttributes() );
+		}
+	}
+}
