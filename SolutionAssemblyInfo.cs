ffd6d8bfe3fa71723e3d3c74437de5494618e3b1
John White <john.t.white@live.com>
2013-07-15 11:03:12 -0400

    Changed version to 1.1.0

--- SolutionAssemblyInfo.cs
+++ SolutionAssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion( "1.0.2.*" )]
-[assembly: AssemblyFileVersion( "1.0.2" )]
+[assembly: AssemblyVersion( "1.1.0.*" )]
+[assembly: AssemblyFileVersion( "1.1.0" )]
