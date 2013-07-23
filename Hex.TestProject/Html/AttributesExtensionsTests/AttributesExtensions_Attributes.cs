fa206e1ef8c2a3dcc5d7629387bf12dab811e6f6
John White <john.t.white@live.com>
2013-07-23 13:31:24 -0400

    Added AttributeExtensions to provide a way to specify attributes on any html element outside of using the HtmlHelper.

---
+++ Hex.TestProject/Html/AttributesExtensionsTests/AttributesExtensions_Attributes.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Hex.Html;
+using System.Collections.Generic;
+
+namespace Hex.TestProject.Html.AttributesExtensionsTests
+{
+	[TestClass]
+	public class AttributesExtensions_Attributes
+	{
+		[TestMethod]
+		public void WithHtmlAttributesObjectReturnsCorrectly()
+		{
+			string attributeValue1 = "AttributeValue1";
+			string attributeValue2 = "AttributeValue2";
+
+			object htmlAttributes = new
+			{
+				AttributeName1 = attributeValue1,
+				AttributeName2 = attributeValue2
+			};
+
+			HtmlHelper htmlHelper = HtmlHelperGenerator.CreateHtmlHelper();
+			var result = htmlHelper.Attributes( htmlAttributes );
+
+			string expectedResult = string.Format( "AttributeName1=\"{0}\" AttributeName2=\"{1}\"", attributeValue1, attributeValue2 );
+			Assert.AreEqual( expectedResult, result.ToHtmlString() );
+		}
+
+		[TestMethod]
+		public void WithHtmlAttributesDictionaryReturnsCorrectly()
+		{
+			string attributeName1 = "AttributeName1";
+			string attributeValue1 = "AttributeValue1";
+
+			string attributeName2 = "AttributeName2";
+			string attributeValue2 = "AttributeValue2";
+
+			Dictionary<string, object> htmlAttributes = new Dictionary<string,object>();
+			htmlAttributes.Add( attributeName1, attributeValue1 );
+			htmlAttributes.Add( attributeName2, attributeValue2 );
+
+			HtmlHelper htmlHelper = HtmlHelperGenerator.CreateHtmlHelper();
+			var result = htmlHelper.Attributes( htmlAttributes );
+
+			string expectedResult = string.Format( "{0}=\"{1}\" {2}=\"{3}\"", attributeName1, attributeValue1, attributeName2, attributeValue2 );
+			Assert.AreEqual( expectedResult, result.ToHtmlString() );
+		}
+
+		[TestMethod]
+		public void WithNullHtmlAttributesDictionaryReturnsCorrectly()
+		{
+			HtmlHelper htmlHelper = HtmlHelperGenerator.CreateHtmlHelper();
+
+			Dictionary<string, object> htmlAttributes = null;
+			var result = htmlHelper.Attributes( htmlAttributes );
+
+			Assert.AreEqual( string.Empty, result.ToHtmlString() );
+		}
+
+		[TestMethod]
+		public void WithAttributeExpressionReturnsCorrectly()
+		{
+			string attributeName1 = "AttributeName1";
+			string attributeValue1 = "AttributeValue1";
+
+			string attributeName2 = "AttributeName2";
+			string attributeValue2 = "AttributeValue2";
+
+			HtmlHelper htmlHelper = HtmlHelperGenerator.CreateHtmlHelper();
+			var result = htmlHelper.Attributes( x => x.Attribute( attributeName1, attributeValue1 ).Attribute( attributeName2, attributeValue2 ) );
+
+			string expectedResult = string.Format( "{0}=\"{1}\" {2}=\"{3}\"", attributeName1, attributeValue1, attributeName2, attributeValue2 );
+			Assert.AreEqual( expectedResult, result.ToHtmlString() );
+		}
+
+		[TestMethod]
+		public void EncodesAndReturnsCorrectly()
+		{
+			string attributeName1 = "AttributeName1";
+			string attributeValue1 = "\"AttributeValue1\"";
+
+			string attributeName2 = "AttributeName2";
+			string attributeValue2 = "\"AttributeValue2\"";
+
+			HtmlHelper htmlHelper = HtmlHelperGenerator.CreateHtmlHelper();
+			var result = htmlHelper.Attributes( x => x.Attribute( attributeName1, attributeValue1 ).Attribute( attributeName2, attributeValue2 ) );
+
+			string expectedResult = string.Format( "{0}=\"{1}\" {2}=\"{3}\"", attributeName1, htmlHelper.AttributeEncode( attributeValue1 ), attributeName2, htmlHelper.AttributeEncode( attributeValue2 ) );
+			Assert.AreEqual( expectedResult, result.ToHtmlString() );
+		}
+	}
+}
---
+++ Hex/Html/AttributesExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Hex.AttributeBuilders;
+using Hex.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Hex.Html
+{
+	public static class AttributesExtensions
+	{
+		public static MvcHtmlString Attributes( this HtmlHelper htmlHelper, object htmlAttributes )
+		{
+			return htmlHelper.Attributes( new RouteValueDictionary( htmlAttributes ) );
+		}
+
+		public static MvcHtmlString Attributes( this HtmlHelper htmlHelper, IDictionary<string, object> htmlAttributes )
+		{
+			if( htmlAttributes == null )
+			{
+				return MvcHtmlString.Empty;
+			}
+
+			var attributeValues = ( from currentValue in htmlAttributes
+									select string.Format( "{0}=\"{1}\"", currentValue.Key, htmlHelper.AttributeEncode( currentValue.Value ) ) )
+									.ToArray();
+
+			return MvcHtmlString.Create( string.Join( " ", attributeValues ) );
+		}
+
+		public static MvcHtmlString Attributes( this HtmlHelper htmlHelper, Action<HtmlAttributeBuilder> attributeExpression )
+		{
+			return htmlHelper.Attributes( attributeExpression.GetAttributes() );
+		}
+	}
+}
