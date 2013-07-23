4f2db870f412143a6ca63627b92ebce88a46dce9
John White <john.t.white@live.com>
2013-07-23 12:16:03 -0400

    Added tests for AttributeCollection.

---
+++ Hex.TestProject/AttributeCollectionTests/AttributeCollection_AddOrRemoveMinimizedAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hex.TestProject.AttributeCollectionTests
+{
+	[TestClass]
+	public class AttributeCollection_AddOrRemoveMinimizedAttribute
+	{
+		[TestMethod]
+		public void WithValueTrueAddsAttributeCorrectly()
+		{
+			string attributeName = "AttributeName";
+			bool value = true;
+
+			var attributeCollection = new AttributeCollection();
+
+			attributeCollection.AddOrRemoveMinimizedAttribute( attributeName, value );
+
+			Assert.AreEqual( attributeName, attributeCollection[ attributeName ] );
+		}
+
+		[TestMethod]
+		public void WithValueFalseDoesNotAddAttribute()
+		{
+			string attributeName = "AttributeName";
+			bool value = false;
+
+			var attributeCollection = new AttributeCollection();
+
+			attributeCollection.AddOrRemoveMinimizedAttribute( attributeName, value );
+
+			Assert.IsFalse( attributeCollection.ContainsKey( attributeName ) );
+		}
+
+		[TestMethod]
+		public void WithValueFalseRemovesAttributeIfAlreadyExists()
+		{
+			string attributeName = "AttributeName";
+
+			var attributeCollection = new AttributeCollection();
+
+			attributeCollection.AddOrRemoveMinimizedAttribute( attributeName, true );
+			attributeCollection.AddOrRemoveMinimizedAttribute( attributeName, false );
+
+			Assert.IsFalse( attributeCollection.ContainsKey( attributeName ) );
+		}
+	}
+}
---
+++ Hex.TestProject/AttributeCollectionTests/AttributeCollection_GetAttributeValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hex.TestProject.AttributeCollectionTests
+{
+	[TestClass]
+	public class AttributeCollection_GetAttributeValue
+	{
+		[TestMethod]
+		public void ReturnsCorrectly()
+		{
+			string attributeName = "AttributeName";
+			DateTime attributeValue = DateTime.Now;
+
+			var attributeCollection = new AttributeCollection();
+			attributeCollection[ attributeName ] = attributeValue;
+
+			var result = attributeCollection.GetAttributeValue<DateTime>( attributeName );
+
+			Assert.AreEqual( result, attributeValue );
+		}
+
+		[TestMethod]
+		public void WhenNotFoundReturnsDefault()
+		{
+			var attributeCollection = new AttributeCollection();
+
+			var result = attributeCollection.GetAttributeValue<DateTime>( "NotFound" );
+
+			Assert.AreEqual( result, DateTime.MinValue );
+		}
+	}
+}
