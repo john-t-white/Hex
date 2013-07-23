d9340db8af84b893213daf4c6eaa94a19f1c11a8
John White <john.t.white@live.com>
2013-07-23 12:23:00 -0400

    Added tests for AttributeCollection, AttributeNameValueCollection and AttributeValueCollection.

---
+++ Hex.TestProject/AttributeNameValueCollectionTests/AttributeNameValueCollection_ToString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hex.TestProject.AttributeNameValueCollectionTests
+{
+	[TestClass]
+	public class AttributeNameValueCollection_ToString
+	{
+		[TestMethod]
+		public void ReturnsCorrectly()
+		{
+			string attributeName1 = "AttributeName1";
+			string attributeValue1 = "AttributeValue1";
+
+			string attributeName2 = "AttributeName2";
+			string attributeValue2 = "AttributeValue2";
+
+			var attributeNameValueCollection = new AttributeNameValueCollection();
+			attributeNameValueCollection.Add( attributeName1, attributeValue1 );
+			attributeNameValueCollection.Add( attributeName2, attributeValue2 );
+
+			var result = attributeNameValueCollection.ToString();
+
+			string expectedResult = string.Format( "{0}:{1};{2}:{3}", attributeName1, attributeValue1, attributeName2, attributeValue2 );
+			Assert.AreEqual( expectedResult, result );
+		}
+
+		[TestMethod]
+		public void WithNoValuesReturnsCorrectly()
+		{
+			var attributeNameValueCollection = new AttributeNameValueCollection();
+
+			var result = attributeNameValueCollection.ToString();
+
+			Assert.AreEqual( string.Empty, result );
+		}
+	}
+}
---
+++ Hex.TestProject/AttributeValueCollectionTests/AttributeValueCollection_ToString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hex.TestProject.AttributeValueCollectionTests
+{
+	[TestClass]
+	public class AttributeValueCollection_ToString
+	{
+		[TestMethod]
+		public void ReturnsCorrectly()
+		{
+			string attributeValue1 = "AttributeValue1";
+			string attributeValue2 = "AttributeValue2";
+
+			var attributeValueCollection = new AttributeValueCollection();
+			attributeValueCollection.Add( attributeValue1 );
+			attributeValueCollection.Add( attributeValue2 );
+
+			var result = attributeValueCollection.ToString();
+
+			string expectedResult = string.Format( "{0} {1}", attributeValue1, attributeValue2 );
+			Assert.AreEqual( expectedResult, result );
+		}
+
+		[TestMethod]
+		public void WithNoValuesReturnsCorrectly()
+		{
+			var attributeValueCollection = new AttributeValueCollection();
+
+			var result = attributeValueCollection.ToString();
+
+			Assert.AreEqual( string.Empty, result );
+		}
+	}
+}
