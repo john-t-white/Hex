691612b931e6adf91af710f000a0b8e228be6f86
John White <john.t.white@live.com>
2013-07-23 10:21:23 -0400

    Added tests for TelephoneExtensions.

---
+++ Hex.TestProject/Html/TelephoneExtensionsTests/TelephoneExtensions_Telephone.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Hex.Html;
+using System.Collections.Generic;
+
+namespace Hex.TestProject.Html.TelephoneExtensionsTests
+{
+	[TestClass]
+	public class TelephoneExtensions_Telephone
+	{
+		[TestMethod]
+		public void WithNameReturnsCorrectly()
+		{
+			string name = "Name";
+
+			HtmlHelper htmlHelper = HtmlHelperGenerator.CreateHtmlHelper();
+
+			var result = htmlHelper.Telephone( name );
+
+			string expectedResult = string.Format( "<input id=\"{0}\" name=\"{0}\" type=\"tel\" value=\"\" />", name );
+			Assert.AreEqual( expectedResult, result.ToHtmlString() );
+		}
+
+		[TestMethod]
+		public void WithNameAndAttributeExpressionReturnsCorrectly()
+		{
+			string name = "Name";
+			string attributeName = "AttributeName";
+			string attributeValue = "AttributeValue";
+
+			HtmlHelper htmlHelper = HtmlHelperGenerator.CreateHtmlHelper();
+
+			var result = htmlHelper.Telephone( name, x => x.Attribute( attributeName, attributeValue ) );
+
+			string expectedResult = string.Format( "<input {0}=\"{1}\" id=\"{2}\" name=\"{2}\" type=\"tel\" value=\"\" />", attributeName, attributeValue, name );
+			Assert.AreEqual( expectedResult, result.ToHtmlString() );
+		}
+
+		[TestMethod]
+		public void WithNameAndValueReturnsCorrectly()
+		{
+			string name = "Name";
+			string value = "(123) 456-7890";
+
+			HtmlHelper htmlHelper = HtmlHelperGenerator.CreateHtmlHelper();
+
+			var result = htmlHelper.Telephone( name, value );
+
+			string expectedResult = string.Format( "<input id=\"{0}\" name=\"{0}\" type=\"tel\" value=\"{1}\" />", name, value );
+			Assert.AreEqual( expectedResult, result.ToHtmlString() );
+		}
+
+		[TestMethod]
+		public void WithNameValueAndHtmlAttributesObjectReturnsCorrectly()
+		{
+			string name = "Name";
+			string value = "(123) 456-7890";
+			string attributeValue = "attributeValue";
+
+			object htmlAttributes = new
+			{
+				AttributeName = attributeValue
+			};
+
+			HtmlHelper htmlHelper = HtmlHelperGenerator.CreateHtmlHelper();
+
+			var result = htmlHelper.Telephone( name, value, htmlAttributes );
+
+			string expectedResult = string.Format( "<input AttributeName=\"{0}\" id=\"{1}\" name=\"{1}\" type=\"tel\" value=\"{2}\" />", attributeValue, name, value );
+			Assert.AreEqual( expectedResult, result.ToHtmlString() );
+		}
+
+		[TestMethod]
+		public void WithNameValueAndHtmlAttributesDictionaryReturnsCorrectly()
+		{
+			string name = "Name";
+			string value = "(123) 456-7890";
+			string attributeName = "attributeName";
+			string attributeValue = "attributeValue";
+
+			Dictionary<string, object> htmlAttributes = new Dictionary<string, object>();
+			htmlAttributes.Add( attributeName, attributeValue );
+
+			HtmlHelper htmlHelper = HtmlHelperGenerator.CreateHtmlHelper();
+
+			var result = htmlHelper.Telephone( name, value, htmlAttributes );
+
+			string expectedResult = string.Format( "<input {0}=\"{1}\" id=\"{2}\" name=\"{2}\" type=\"tel\" value=\"{3}\" />", attributeName, attributeValue, name, value );
+			Assert.AreEqual( expectedResult, result.ToHtmlString() );
+		}
+
+		[TestMethod]
+		public void WithNameValueAndAttributeExpressionReturnsCorrectly()
+		{
+			string name = "Name";
+			string value = "(123) 456-7890";
+			string attributeName = "attributeName";
+			string attributeValue = "attributeValue";
+
+			HtmlHelper htmlHelper = HtmlHelperGenerator.CreateHtmlHelper();
+
+			var result = htmlHelper.Telephone( name, value, x => x.Attribute( attributeName, attributeValue ) );
+
+			string expectedResult = string.Format( "<input {0}=\"{1}\" id=\"{2}\" name=\"{2}\" type=\"tel\" value=\"{3}\" />", attributeName, attributeValue, name, value );
+			Assert.AreEqual( expectedResult, result.ToHtmlString() );
+		}
+	}
+}
---
+++ Hex.TestProject/Html/TelephoneExtensionsTests/TelephoneViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hex.TestProject.Html.TelephoneExtensionsTests
+{
+	public class TelephoneViewModel
+	{
+		public TelephoneViewModel( string telephoneNumber )
+		{
+			this.TelephoneNumber = telephoneNumber;
+		}
+
+		public string TelephoneNumber { get; set; }
+	}
+}
