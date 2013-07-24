7025a6b0c153408a4f415ca6fa8e31f037ddb4f4
John White <john.t.white@live.com>
2013-07-24 14:55:56 -0400

    Added FormExtensions.BeginForm for AjaxHelper.

---
+++ Hex.TestProject/Ajax/FormExtensionsTests/FormExtensions_BeginForm.cs
@@ -0,0 +1,134 @@
+﻿using Hex.Ajax;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Web.Routing;
+
+namespace Hex.TestProject.Ajax.FormExtensionsTests
+{
+	[TestClass]
+	public class FormExtensions_BeginForm
+	{
+		[TestMethod]
+		public void WithActionNameAjaxOptionsAndAttributeExpressionReturnsCorrectly()
+		{
+			string attributeName = "AttributeName";
+			string attributeValue = "AttributeValue";
+			string actionName = "ActionName";
+			AjaxOptions ajaxOptions = new AjaxOptions();
+
+			AjaxHelper ajaxHelper = AjaxHelperGenerator.CreateAjaxHelper();
+
+			var result = ajaxHelper.BeginForm( actionName, ajaxOptions, x => x.Attribute( attributeName, attributeValue ) );
+
+			string expectedResult = string.Format( "<form {0}=\"{1}\" action=\"/{2}/{3}\" method=\"post\" onclick=\"Sys.Mvc.AsyncForm.handleClick(this, new Sys.UI.DomEvent(event));\" onsubmit=\"Sys.Mvc.AsyncForm.handleSubmit(this, new Sys.UI.DomEvent(event), {{ insertionMode: Sys.Mvc.InsertionMode.replace }});\">", attributeName, attributeValue, AjaxHelperGenerator.DefaultController, actionName );
+			Assert.AreEqual( expectedResult, ajaxHelper.ViewContext.Writer.ToString() );
+		}
+
+		[TestMethod]
+		public void WithActionNameRouteValuesObjectAjaxOptionsAndAttributeExpressionReturnsCorrectly()
+		{
+			string attributeName = "AttributeName";
+			string attributeValue = "AttributeValue";
+			string actionName = "ActionName";
+			AjaxOptions ajaxOptions = new AjaxOptions();
+			string routeValue = "RouteValue";
+
+			object routeValues = new
+			{
+				RouteParameter = routeValue
+			};
+
+			AjaxHelper ajaxHelper = AjaxHelperGenerator.CreateAjaxHelper();
+
+			var result = ajaxHelper.BeginForm( actionName, routeValues, ajaxOptions, x => x.Attribute( attributeName, attributeValue ) );
+
+			string expectedResult = string.Format( "<form {0}=\"{1}\" action=\"/{2}/{3}?RouteParameter={4}\" method=\"post\" onclick=\"Sys.Mvc.AsyncForm.handleClick(this, new Sys.UI.DomEvent(event));\" onsubmit=\"Sys.Mvc.AsyncForm.handleSubmit(this, new Sys.UI.DomEvent(event), {{ insertionMode: Sys.Mvc.InsertionMode.replace }});\">", attributeName, attributeValue, AjaxHelperGenerator.DefaultController, actionName, routeValue );
+			Assert.AreEqual( expectedResult, ajaxHelper.ViewContext.Writer.ToString() );
+		}
+
+		[TestMethod]
+		public void WithActionNameRouteValuesDictionaryAjaxOptionsAndAttributeExpressionReturnsCorrectly()
+		{
+			string attributeName = "AttributeName";
+			string attributeValue = "AttributeValue";
+			string actionName = "ActionName";
+			AjaxOptions ajaxOptions = new AjaxOptions();
+			string routeParameter = "RouteParameter";
+			string routeValue = "RouteValue";
+
+			RouteValueDictionary routeValues = new RouteValueDictionary();
+			routeValues.Add( routeParameter, routeValue );
+
+			AjaxHelper ajaxHelper = AjaxHelperGenerator.CreateAjaxHelper();
+
+			var result = ajaxHelper.BeginForm( actionName, routeValues, ajaxOptions, x => x.Attribute( attributeName, attributeValue ) );
+
+			string expectedResult = string.Format( "<form {0}=\"{1}\" action=\"/{2}/{3}?{4}={5}\" method=\"post\" onclick=\"Sys.Mvc.AsyncForm.handleClick(this, new Sys.UI.DomEvent(event));\" onsubmit=\"Sys.Mvc.AsyncForm.handleSubmit(this, new Sys.UI.DomEvent(event), {{ insertionMode: Sys.Mvc.InsertionMode.replace }});\">", attributeName, attributeValue, AjaxHelperGenerator.DefaultController, actionName, routeParameter, routeValue );
+			Assert.AreEqual( expectedResult, ajaxHelper.ViewContext.Writer.ToString() );
+		}
+
+		[TestMethod]
+		public void WithActionNameControllerNameAjaxOptionsAndAttributeExpressionReturnsCorrectly()
+		{
+			string attributeName = "AttributeName";
+			string attributeValue = "AttributeValue";
+			string actionName = "ActionName";
+			string controllerName = "ControllerName";
+			AjaxOptions ajaxOptions = new AjaxOptions();
+
+			AjaxHelper ajaxHelper = AjaxHelperGenerator.CreateAjaxHelper();
+
+			var result = ajaxHelper.BeginForm( actionName, controllerName, ajaxOptions, x => x.Attribute( attributeName, attributeValue ) );
+
+			string expectedResult = string.Format( "<form {0}=\"{1}\" action=\"/{2}/{3}\" method=\"post\" onclick=\"Sys.Mvc.AsyncForm.handleClick(this, new Sys.UI.DomEvent(event));\" onsubmit=\"Sys.Mvc.AsyncForm.handleSubmit(this, new Sys.UI.DomEvent(event), {{ insertionMode: Sys.Mvc.InsertionMode.replace }});\">", attributeName, attributeValue, controllerName, actionName );
+			Assert.AreEqual( expectedResult, ajaxHelper.ViewContext.Writer.ToString() );
+		}
+
+		[TestMethod]
+		public void WithActionNameControllerNameRouteValuesObjectAjaxOptionsAndAttributeExpressionReturnsCorrectly()
+		{
+			string attributeName = "AttributeName";
+			string attributeValue = "AttributeValue";
+			string actionName = "ActionName";
+			string controllerName = "ControllerName";
+			AjaxOptions ajaxOptions = new AjaxOptions();
+			string routeValue = "RouteValue";
+
+			object routeValues = new
+			{
+				RouteParameter = routeValue
+			};
+
+			AjaxHelper ajaxHelper = AjaxHelperGenerator.CreateAjaxHelper();
+
+			var result = ajaxHelper.BeginForm( actionName, controllerName, routeValues, ajaxOptions, x => x.Attribute( attributeName, attributeValue ) );
+
+			string expectedResult = string.Format( "<form {0}=\"{1}\" action=\"/{2}/{3}?RouteParameter={4}\" method=\"post\" onclick=\"Sys.Mvc.AsyncForm.handleClick(this, new Sys.UI.DomEvent(event));\" onsubmit=\"Sys.Mvc.AsyncForm.handleSubmit(this, new Sys.UI.DomEvent(event), {{ insertionMode: Sys.Mvc.InsertionMode.replace }});\">", attributeName, attributeValue, controllerName, actionName, routeValue );
+			Assert.AreEqual( expectedResult, ajaxHelper.ViewContext.Writer.ToString() );
+		}
+
+		[TestMethod]
+		public void WithActionNameControllerNameRouteValuesDictionaryAjaxOptionsAndAttributeExpressionReturnsCorrectly()
+		{
+			string attributeName = "AttributeName";
+			string attributeValue = "AttributeValue";
+			string actionName = "ActionName";
+			string controllerName = "ControllerName";
+			AjaxOptions ajaxOptions = new AjaxOptions();
+			string routeParameter = "RouteParameter";
+			string routeValue = "RouteValue";
+
+			RouteValueDictionary routeValues = new RouteValueDictionary();
+			routeValues.Add( routeParameter, routeValue );
+
+			AjaxHelper ajaxHelper = AjaxHelperGenerator.CreateAjaxHelper();
+
+			var result = ajaxHelper.BeginForm( actionName, controllerName, routeValues, ajaxOptions, x => x.Attribute( attributeName, attributeValue ) );
+
+			string expectedResult = string.Format( "<form {0}=\"{1}\" action=\"/{2}/{3}?{4}={5}\" method=\"post\" onclick=\"Sys.Mvc.AsyncForm.handleClick(this, new Sys.UI.DomEvent(event));\" onsubmit=\"Sys.Mvc.AsyncForm.handleSubmit(this, new Sys.UI.DomEvent(event), {{ insertionMode: Sys.Mvc.InsertionMode.replace }});\">", attributeName, attributeValue, controllerName, actionName, routeParameter, routeValue );
+			Assert.AreEqual( expectedResult, ajaxHelper.ViewContext.Writer.ToString() );
+		}
+	}
+}
---
+++ Hex/Ajax/FormExtensions.cs
@@ -0,0 +1,112 @@
+﻿using Hex.AttributeBuilders;
+using Hex.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace Hex.Ajax
+{
+	/// <summary>
+	/// Represents support for ASP.NET AJAX form within an ASP.NET MVC application with an expression for specifying HTML attributes.
+	/// </summary>
+	public static class FormExtensions
+	{
+		/// <summary>
+		/// Writes an opening &lt;form&gt; tag to the response.
+		/// </summary>
+		/// <param name="ajaxHelper">The AJAX helper.</param>
+		/// <param name="actionName">The name of the action method that will handle the request.</param>
+		/// <param name="ajaxOptions">An object that provides options for the asynchronous request.</param>
+		/// <param name="attributeExpression">An expression that contains the HTML attributes to set for the element.</param>
+		/// <returns>An opening &lt;form&gt; tag.</returns>
+		public static MvcForm BeginForm( this AjaxHelper ajaxHelper, string actionName, AjaxOptions ajaxOptions, Action<HtmlAttributeBuilder> attributeExpression )
+		{
+			return ajaxHelper.BeginForm( actionName, null, null, ajaxOptions, attributeExpression );
+		}
+
+		/// <summary>
+		/// Writes an opening &lt;form&gt; tag to the response.
+		/// </summary>
+		/// <param name="ajaxHelper">The AJAX helper.</param>
+		/// <param name="actionName">The name of the action method that will handle the request.</param>
+		/// <param name="routeValues">
+		///		An object that contains the parameters for a route.
+		///		The parameters are retrieved through reflection by examining the properties of the object.
+		///		This object is typically created by using object initializer syntax.
+		/// </param>
+		/// <param name="ajaxOptions">An object that provides options for the asynchronous request.</param>
+		/// <param name="attributeExpression">An expression that contains the HTML attributes to set for the element.</param>
+		/// <returns>An opening &lt;form&gt; tag.</returns>
+		public static MvcForm BeginForm( this AjaxHelper ajaxHelper, string actionName, object routeValues, AjaxOptions ajaxOptions, Action<HtmlAttributeBuilder> attributeExpression )
+		{
+			return ajaxHelper.BeginForm( actionName, null, new RouteValueDictionary( routeValues ), ajaxOptions, attributeExpression );
+		}
+
+		/// <summary>
+		/// Writes an opening &lt;form&gt; tag to the response.
+		/// </summary>
+		/// <param name="ajaxHelper">The AJAX helper.</param>
+		/// <param name="actionName">The name of the action method that will handle the request.</param>
+		/// <param name="routeValues">An object that contains the parameters for a route.</param>
+		/// <param name="ajaxOptions">An object that provides options for the asynchronous request.</param>
+		/// <param name="attributeExpression">An expression that contains the HTML attributes to set for the element.</param>
+		/// <returns>An opening &lt;form&gt; tag.</returns>
+		public static MvcForm BeginForm( this AjaxHelper ajaxHelper, string actionName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions, Action<HtmlAttributeBuilder> attributeExpression )
+		{
+			return ajaxHelper.BeginForm( actionName, null, routeValues, ajaxOptions, attributeExpression );
+		}
+
+		/// <summary>
+		/// Writes an opening &lt;form&gt; tag to the response.
+		/// </summary>
+		/// <param name="ajaxHelper">The AJAX helper.</param>
+		/// <param name="actionName">The name of the action method that will handle the request.</param>
+		/// <param name="controllerName">The name of the controller.</param>
+		/// <param name="ajaxOptions">An object that provides options for the asynchronous request.</param>
+		/// <param name="attributeExpression">An expression that contains the HTML attributes to set for the element.</param>
+		/// <returns>An opening &lt;form&gt; tag.</returns>
+		public static MvcForm BeginForm( this AjaxHelper ajaxHelper, string actionName, string controllerName, AjaxOptions ajaxOptions, Action<HtmlAttributeBuilder> attributeExpression )
+		{
+			return ajaxHelper.BeginForm( actionName, controllerName, null, ajaxOptions, attributeExpression );
+		}
+
+		/// <summary>
+		/// Writes an opening &lt;form&gt; tag to the response.
+		/// </summary>
+		/// <param name="ajaxHelper">The AJAX helper.</param>
+		/// <param name="actionName">The name of the action method that will handle the request.</param>
+		/// <param name="controllerName">The name of the controller.</param>
+		/// <param name="routeValues">
+		///		An object that contains the parameters for a route.
+		///		The parameters are retrieved through reflection by examining the properties of the object.
+		///		This object is typically created by using object initializer syntax.
+		/// </param>
+		/// <param name="ajaxOptions">An object that provides options for the asynchronous request.</param>
+		/// <param name="attributeExpression">An expression that contains the HTML attributes to set for the element.</param>
+		/// <returns>An opening &lt;form&gt; tag.</returns>
+		public static MvcForm BeginForm( this AjaxHelper ajaxHelper, string actionName, string controllerName, object routeValues, AjaxOptions ajaxOptions, Action<HtmlAttributeBuilder> attributeExpression )
+		{
+			return ajaxHelper.BeginForm( actionName, controllerName, new RouteValueDictionary( routeValues ), ajaxOptions, attributeExpression );
+		}
+
+		/// <summary>
+		/// Writes an opening &lt;form&gt; tag to the response.
+		/// </summary>
+		/// <param name="ajaxHelper">The AJAX helper.</param>
+		/// <param name="actionName">The name of the action method that will handle the request.</param>
+		/// <param name="controllerName">The name of the controller.</param>
+		/// <param name="routeValues">An object that contains the parameters for a route.</param>
+		/// <param name="ajaxOptions">An object that provides options for the asynchronous request.</param>
+		/// <param name="attributeExpression">An expression that contains the HTML attributes to set for the element.</param>
+		/// <returns>An opening &lt;form&gt; tag.</returns>
+		public static MvcForm BeginForm( this AjaxHelper ajaxHelper, string actionName, string controllerName, RouteValueDictionary routeValues, AjaxOptions ajaxOptions, Action<HtmlAttributeBuilder> attributeExpression )
+		{
+			return ajaxHelper.BeginForm( actionName, controllerName, routeValues, ajaxOptions, attributeExpression.GetAttributes() );
+		}
+	}
+}
