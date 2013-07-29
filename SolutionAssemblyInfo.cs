5d5d885961fdf664b4e46cb191aa189e382c39f7
John White <john.t.white@live.com>
2013-07-29 08:35:44 -0400

    Updated version to 2.0.0

--- SolutionAssemblyInfo.cs
+++ SolutionAssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion( "1.1.1.*" )]
-[assembly: AssemblyFileVersion( "1.1.1" )]
+[assembly: AssemblyVersion( "2.0.0.*" )]
+[assembly: AssemblyFileVersion( "2.0.0" )]
