689a3132c2083484d0e3bbff5e1074c636ad8a30
John White <john.t.white@live.com>
2013-07-31 11:02:27 -0400

    Added tests for StyleCollection.

---
+++ Hex.TestProject/StyleCollectionTests/StyleCollection_ToString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hex.TestProject.AttributeNameValueCollectionTests
+{
+	[TestClass]
+	public class AttributeNameValueCollection_ToString
+	{
+		[TestMethod]
+		public void ReturnsCorrectly()
+		{
+			string definitionName1 = "DefinitionName1";
+			string definitionValue1 = "DefinitionValue1";
+
+			string definitionName2 = "DefinitionName2";
+			string definitionValue2 = "DefinitionValue2";
+
+			var styles = new StyleCollection();
+			styles.Add( definitionName1, definitionValue1 );
+			styles.Add( definitionName2, definitionValue2 );
+
+			var result = styles.ToString();
+
+			string expectedResult = string.Format( "{0}:{1};{2}:{3}", definitionName1, definitionValue1, definitionName2, definitionValue2 );
+			Assert.AreEqual( expectedResult, result );
+		}
+
+		[TestMethod]
+		public void WithNoValuesReturnsCorrectly()
+		{
+			var styles = new StyleCollection();
+
+			var result = styles.ToString();
+
+			Assert.AreEqual( string.Empty, result );
+		}
+	}
+}
