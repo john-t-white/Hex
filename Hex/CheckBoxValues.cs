66572e4c8ce82a970d4e3757ce27dbe3a574523b
John White <john.t.white@live.com>
2013-08-23 10:49:20 -0400

    Added missing file.

---
+++ Hex/CheckBoxValues.cs
@@ -0,0 +1,68 @@
+﻿using Hex.ModelBinders;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Hex
+{
+	/// <summary>
+	/// Contains the list of checkboxes that are selected in a checkbox list.
+	/// </summary>
+	[ModelBinder( typeof( CheckBoxValuesModelBinder ) )]
+	public class CheckBoxValues
+		: ArrayList
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Hex.CheckBoxValues" /> class.
+		/// </summary>
+		public CheckBoxValues()
+			: base()
+		{ }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Hex.CheckBoxValues" /> class.
+		/// </summary>
+		public CheckBoxValues( ICollection collection )
+			: base( collection )
+		{ }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Hex.CheckBoxValues" /> class.
+		/// </summary>
+		public CheckBoxValues( int capacity )
+			: base( capacity )
+		{ }
+	}
+
+	/// <summary>
+	/// Contains the list of checkboxes that are selected in a checkbox list.
+	/// </summary>
+	[ModelBinder( typeof( CheckBoxValuesModelBinder ) )]
+	public class CheckBoxValues<T>
+		: List<T>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Hex.CheckBoxValues{T}" /> class.
+		/// </summary>
+		public CheckBoxValues()
+			: base()
+		{ }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Hex.CheckBoxValues{T}" /> class.
+		/// </summary>
+		public CheckBoxValues( int capacity )
+			: base( capacity )
+		{ }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Hex.CheckBoxValues{T}" /> class.
+		/// </summary>
+		public CheckBoxValues( IEnumerable<T> collection )
+			: base( collection )
+		{ }
+	}
+}
