bbaacba494cfb9bf109e4682399784caae914690
John White <john.t.white@live.com>
2013-09-30 14:48:54 -0400

    Added Update extension to ModelStateDictionary to replace the current model state values with the values from a WizardStepValueCollection.

---
+++ Hex.TestProject/Extensions/ModelStateDictionaryExtensionsTests/ModelStateDictionaryExtensions_Update.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using System.Globalization;
+using Hex.Wizard;
+using Hex.Extensions;
+
+namespace Hex.TestProject.Extensions.ModelStateDictionaryExtensionsTests
+{
+	[TestClass]
+	public class ModelStateDictionaryExtensions_Update
+	{
+		[TestMethod]
+		public void UpdatesCorrectly()
+		{
+			string[] wizardStepValueNames = new string[]
+			{
+				"NameOne",
+				"NameTwo"
+			};
+
+			WizardStepValueCollection wizardStepValues = new WizardStepValueCollection();
+			foreach( string currentName in wizardStepValueNames )
+			{
+				for( int currentValueIndex = 0; currentValueIndex < 2; currentValueIndex++ )
+				{
+					string currentValue = string.Format( "{0}Value{1}", currentName, currentValueIndex );
+					wizardStepValues.Add( currentName, currentValue );
+				}
+			}
+
+			ModelStateDictionary modelStateDictionary = new ModelStateDictionary();
+			modelStateDictionary.SetModelValue( "Key", new ValueProviderResult( "RawValue", "AttemptedValue", CultureInfo.CurrentUICulture ) );
+
+			modelStateDictionary.Update( wizardStepValues );
+
+			Assert.AreEqual( wizardStepValueNames.Length, modelStateDictionary.Count );
+			foreach( string currentName in wizardStepValueNames )
+			{
+				Assert.IsTrue( modelStateDictionary.ContainsKey( currentName ) );
+				ValueProviderResult currentValueProviderResult = modelStateDictionary[ currentName ].Value;
+
+				Assert.IsNotNull( currentValueProviderResult );
+
+				string[] expectedValues = wizardStepValues.GetValues( currentName );
+				string expectedAttemptedValue = string.Join( ",", expectedValues );
+				string[] currentRawValues = ( string[] )currentValueProviderResult.RawValue;
+
+				Assert.AreEqual( expectedValues.Length, currentRawValues.Length );
+				foreach( string currentExpectedValue in expectedValues )
+				{
+					Assert.IsTrue( currentRawValues.Contains( currentExpectedValue ) );
+				}
+
+				Assert.AreEqual( expectedAttemptedValue, currentValueProviderResult.AttemptedValue );
+				Assert.AreEqual( CultureInfo.CurrentUICulture, currentValueProviderResult.Culture );
+			}
+		}
+
+		[TestMethod]
+		public void WithNullValuesUpdatesCorrectly()
+		{
+			WizardStepValueCollection wizardStepValues = null;
+
+			ModelStateDictionary modelStateDictionary = new ModelStateDictionary();
+			modelStateDictionary.SetModelValue( "Key", new ValueProviderResult( "RawValue", "AttemptedValue", CultureInfo.CurrentUICulture ) );
+
+			modelStateDictionary.Update( wizardStepValues );
+
+			Assert.AreEqual( 0, modelStateDictionary.Count );
+		}
+	}
+}
--- Hex/Extensions/ModelStateDictionaryExtensions.cs
+++ Hex/Extensions/ModelStateDictionaryExtensions.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Hex.Wizard;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
@@ -27,6 +30,73 @@ namespace Hex.Extensions
 			}
 
 			return modelState.Value.ConvertTo( destinationType, null );
+		}
+
+
+
+		//public static WizardStepValueCollection ToWizardStepValueCollection( this ModelStateDictionary modelStateDictionary, IValueProvider valueProvider )
+		//{
+		//	WizardStepValueCollection wizardStepValues = new WizardStepValueCollection();
+
+		//	foreach( KeyValuePair<string, ModelState> currentModelState in modelStateDictionary )
+		//	{
+		//		object rawValue = currentModelState.Value.Value.RawValue;
+		//		if( rawValue is IEnumerable )
+		//		{
+		//			foreach( var currentRawValue in ( IEnumerable )rawValue )
+		//			{
+		//				wizardStepValues.Add( currentModelState.Key, currentRawValue.ToString() );
+		//			}
+		//		}
+		//		else
+		//		{
+		//			wizardStepValues.Add( currentModelState.Key, rawValue.ToString() );
+		//		}
+		//	}
+
+		//	var possibleIndexes = ( from string currentModelStateName in wizardStepValues
+		//							let bracketIndex = currentModelStateName.LastIndexOf( '[' )
+		//							where bracketIndex >= 0
+		//							select string.Concat( currentModelStateName.Substring( 0, bracketIndex ), ".index" ) ).Distinct().ToList();
+
+		//	foreach( string currentPossibleIndex in possibleIndexes )
+		//	{
+		//		ValueProviderResult currentValueProviderResult = valueProvider.GetValue( currentPossibleIndex );
+		//		if( currentValueProviderResult != null )
+		//		{
+		//			object rawValue = currentValueProviderResult.RawValue;
+		//			if( rawValue is IEnumerable )
+		//			{
+		//				foreach( var currentRawValue in ( IEnumerable )rawValue )
+		//				{
+		//					wizardStepValues.Add( currentPossibleIndex, currentRawValue.ToString() );
+		//				}
+		//			}
+		//			else
+		//			{
+		//				wizardStepValues.Add( currentPossibleIndex, rawValue.ToString() );
+		//			}
+		//		}
+		//	}
+
+		//	return wizardStepValues;
+		//}
+
+		public static void Update( this ModelStateDictionary modelStateDictionary, WizardStepValueCollection values )
+		{
+			modelStateDictionary.Clear();
+
+			if( values != null )
+			{
+				foreach( string currentName in values )
+				{
+					string[] rawValues = values.GetValues( currentName );
+					string attemptedValue = string.Join( ",", rawValues );
+					ValueProviderResult valueProviderResult = new ValueProviderResult( rawValues, attemptedValue, CultureInfo.CurrentUICulture );
+
+					modelStateDictionary.SetModelValue( currentName, valueProviderResult );
+				}
+			}
 		}
 	}
 }
