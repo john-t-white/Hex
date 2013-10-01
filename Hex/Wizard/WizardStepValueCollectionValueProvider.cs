2ff10936cdafec5b1a31a1b3d00d8134514a8380
John White <john.t.white@live.com>
2013-10-01 14:11:50 -0400

    Added WizardStepValueCollectionValueProvider.

---
+++ Hex/Wizard/WizardStepValueCollectionValueProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Hex.Wizard
+{
+	public class WizardStepValueCollectionValueProvider
+		: NameValueCollectionValueProvider
+	{
+		public WizardStepValueCollectionValueProvider( WizardStepValueCollection wizardStepValueCollection, CultureInfo culture )
+			: base( wizardStepValueCollection, culture )
+		{ }
+	}
+}
