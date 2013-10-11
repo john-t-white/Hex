cff34c5ec0668f33b940727dad2c1c5eef1e5dcb
John White <john.t.white@live.com>
2013-10-11 10:13:27 -0400

    Added tests for CreateWizardFormModelLifeCycleCommand.

---
+++ Hex.TestProject/Wizard/LifeCycle/CreateWizardFormModelLifeCycleCommandTests/CreateWizardFormModelLifeCycleCommand_Execute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hex.Wizard.LifeCycle;
+
+namespace Hex.TestProject.Wizard.LifeCycle.CreateWizardFormModelLifeCycleCommandTests
+{
+	[TestClass]
+	public class CreateWizardFormModelLifeCycleCommand_Execute
+	{
+		[TestMethod]
+		public void CreatesAndSetsWizardFormModelCorrectly()
+		{
+			FakeWizardControllerWithNoActions wizardController = new FakeWizardControllerWithNoActions();
+			WizardLifeCycleContext lifeCycleContext = new WizardLifeCycleContext( wizardController );
+
+			CreateWizardFormModelLifeCycleCommand lifeCycleCommand = new CreateWizardFormModelLifeCycleCommand();
+			lifeCycleCommand.Execute( lifeCycleContext );
+
+			Assert.IsNotNull( wizardController.WizardFormModel );
+			Assert.IsInstanceOfType( wizardController.WizardFormModel, wizardController.WizardFormModelType );
+		}
+	}
+}
