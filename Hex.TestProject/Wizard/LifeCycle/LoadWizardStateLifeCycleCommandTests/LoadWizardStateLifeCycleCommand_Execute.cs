f06cdb132e8a1a0a06ae2f42b741eeddd1eeee78
John White <john.t.white@live.com>
2013-10-11 14:43:53 -0400

    Added tests for LoadWizardStateLifeCycleCommand

---
+++ Hex.TestProject/Wizard/LifeCycle/LoadWizardStateLifeCycleCommandTests/LoadWizardStateLifeCycleCommand_Execute.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hex.Wizard.LifeCycle;
+using System.Web.Routing;
+using System.Web.Mvc;
+using Hex.Wizard;
+using Rhino.Mocks;
+
+namespace Hex.TestProject.Wizard.LifeCycle.LoadWizardStateLifeCycleCommandTests
+{
+	[TestClass]
+	public class LoadWizardStateLifeCycleCommand_Execute
+	{
+		[TestMethod]
+		public void LoadsWizardStateCorrectly()
+		{
+			string wizardStateToken = "WizardStateToken";
+
+			WizardState wizardState = new WizardState( "ActionName", new WizardStepState[] { new WizardStepState( "ActionName", null ) } );
+
+			WizardController wizardController = this.GenerateWizardController( wizardStateToken, wizardState );
+
+			WizardLifeCycleContext lifeCycleContext = new WizardLifeCycleContext( wizardController );
+
+			LoadWizardStateLifeCycleCommand lifeCycleCommand = new LoadWizardStateLifeCycleCommand( wizardStateToken );
+			lifeCycleCommand.Execute( lifeCycleContext );
+
+			Assert.IsNull( lifeCycleContext.ResultActionName );
+			Assert.AreSame( wizardState, lifeCycleContext.WizardState );
+		}
+
+		[TestMethod]
+		public void WithWizardStateNotFoundSetsResultActionNameCorrectly()
+		{
+			string wizardStateToken = "WizardStateToken";
+
+			WizardState wizardState = null;
+
+			WizardController wizardController = this.GenerateWizardController( wizardStateToken, wizardState );
+
+			WizardLifeCycleContext lifeCycleContext = new WizardLifeCycleContext( wizardController );
+
+			LoadWizardStateLifeCycleCommand lifeCycleCommand = new LoadWizardStateLifeCycleCommand( wizardStateToken );
+			lifeCycleCommand.Execute( lifeCycleContext );
+
+			Assert.AreEqual( "HandleWizardStateNotFound", lifeCycleContext.ResultActionName );
+			Assert.IsNull( lifeCycleContext.WizardState );
+		}
+
+
+
+		private WizardController GenerateWizardController( string wizardStateToken, WizardState expectedWizardState )
+		{
+			RequestContext requestContext = new RequestContext();
+			FakeWizardControllerWithThreeSteps wizardController = new FakeWizardControllerWithThreeSteps();
+			ControllerContext controllerContext = new ControllerContext( requestContext, wizardController );
+			wizardController.ControllerContext = controllerContext;
+
+			IWizardStateProvider wizardStateProvider = MockRepository.GenerateMock<IWizardStateProvider>();
+			wizardStateProvider.Expect( x => x.Load( controllerContext, wizardStateToken ) )
+				.Return( expectedWizardState );
+
+			wizardController.WizardStateProvider = wizardStateProvider;
+
+			return wizardController;
+		}
+	}
+}
---
+++ Hex.TestProject/Wizard/LifeCycle/LoadWizardStateLifeCycleCommandTests/LoadWizardStateLifeCycleCommand_ctor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hex.Wizard.LifeCycle;
+
+namespace Hex.TestProject.Wizard.LifeCycle.LoadWizardStateLifeCycleCommandTests
+{
+	[TestClass]
+	public class LoadWizardStateLifeCycleCommand_ctor
+	{
+		[TestMethod]
+		public void CreatesCorrectly()
+		{
+			string wizardStateToken = "WizardStateToken";
+
+			LoadWizardStateLifeCycleCommand lifeCycleCommand = new LoadWizardStateLifeCycleCommand( wizardStateToken );
+
+			Assert.AreEqual( wizardStateToken, lifeCycleCommand.WizardStateToken );
+		}
+
+		[TestMethod]
+		[ExpectedException( typeof( ArgumentException ) )]
+		public void WithNullWizardStateTokenThrowsArgumentException()
+		{
+			string wizardStateToken = null;
+
+			new LoadWizardStateLifeCycleCommand( wizardStateToken );
+		}
+
+		[TestMethod]
+		[ExpectedException( typeof( ArgumentException ) )]
+		public void WithEmptyWizardStateTokenThrowsArgumentException()
+		{
+			string wizardStateToken = string.Empty;
+
+			new LoadWizardStateLifeCycleCommand( wizardStateToken );
+		}
+
+		[TestMethod]
+		[ExpectedException( typeof( ArgumentException ) )]
+		public void WithWhitespaceWizardStateTokenThrowsArgumentException()
+		{
+			string wizardStateToken = " ";
+
+			new LoadWizardStateLifeCycleCommand( wizardStateToken );
+		}
+	}
+}
