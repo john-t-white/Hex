f88e80214662f083c2a8234129f65162f0b96f8c
John White <john.t.white@live.com>
2013-10-14 10:49:01 -0400

    Added tests for WizardLifeCycleContext.

---
+++ Hex.TestProject/Wizard/LifeCycle/WizardLifeCycleContextTests/WizardLifeCycleContext_ctor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hex.Wizard.LifeCycle;
+using Hex.Wizard;
+
+namespace Hex.TestProject.Wizard.LifeCycle.WizardLifeCycleContextTests
+{
+	[TestClass]
+	public class WizardLifeCycleContext_ctor
+	{
+		[TestMethod]
+		public void CreatesCorrectly()
+		{
+			FakeWizardControllerWithNoActions wizardController = new FakeWizardControllerWithNoActions();
+
+			WizardLifeCycleContext wizardLifeCycleContext = new WizardLifeCycleContext( wizardController );
+
+			Assert.AreSame( wizardController, wizardLifeCycleContext.WizardController );
+		}
+
+		[TestMethod]
+		[ExpectedException( typeof( ArgumentNullException ) )]
+		public void WithNullWizardControllerThrowsArgumentNullException()
+		{
+			WizardController wizardController = null;
+
+			new WizardLifeCycleContext( wizardController );
+		}
+	}
+}
